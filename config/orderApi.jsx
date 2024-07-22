749350bffb54ca1663ea1952243e40839c1962d4
giraffenostrils <dreamthre@gamil.com>
2024-07-22 20:10:02 +0900

    order get api

--- config/orderApi.jsx
+++ config/orderApi.jsx
@@ -1,40 +1,51 @@
 import { orderApi } from '../config/orderNetwork';
 
-    export const createOrder = async (data) => {
-        const userRequestDto = {
-            customerId: data.customerId,
-            ownerId: data.ownerId,
-            changeTime: data.changeTime,
-            orderState: data.orderState,
-            orderCreatedAt: data.orderCreatedAt,
-            storePhone: data.storePhone,
-            storeName: data.storeName,
-            storeAddressString: data.storeAddressString,
-            menuName: data.menuName,
-            menuIntroduction: data.menuIntroduction,
-            menuPrice: data.menuPrice,
-            optionTitle: data.optionTitle,
-            optionPrice: data.optionPrice,
-            listName: data.listName,
-            options: data.options,
-            customerRequests: data.customerRequests,
-            riderRequests: data.riderRequests,
-            order: data.order,
-            orderTotalAmount: data.orderTotalAmount,
-            storeDeliveryFee: data.storeDeliveryFee,
-            deliveryFee: data.deliveryFee,
-            distanceFromStoreToCustomer: data.distanceFromStoreToCustomer,
-            storeLongitude: data.storeLongitude,
-            storeLatitude: data.storeLatitude,
-            due: data.due,
-            menuNameList: data.menuNameList
-        };
-    
-        try {
-            const res = await orderApi('api/v1/Order/history', 'post', userRequestDto);
-            return res.data; // Return the response data
-        } catch (error) {
-            console.error('Error creating order:', error);
-            throw error; // Re-throw the error for further handling
-        }
+export const createOrder = async (data) => {
+    const userRequestDto = {
+        customerId: data.customerId,
+        ownerId: data.ownerId,
+        changeTime: data.changeTime,
+        orderState: data.orderState,
+        orderCreatedAt: data.orderCreatedAt,
+        storePhone: data.storePhone,
+        storeName: data.storeName,
+        storeAddressString: data.storeAddressString,
+        menuName: data.menuName,
+        menuIntroduction: data.menuIntroduction,
+        menuPrice: data.menuPrice,
+        optionTitle: data.optionTitle,
+        optionPrice: data.optionPrice,
+        listName: data.listName,
+        options: data.options,
+        customerRequests: data.customerRequests,
+        riderRequests: data.riderRequests,
+        order: data.order,
+        orderTotalAmount: data.orderTotalAmount,
+        storeDeliveryFee: data.storeDeliveryFee,
+        deliveryFee: data.deliveryFee,
+        distanceFromStoreToCustomer: data.distanceFromStoreToCustomer,
+        storeLongitude: data.storeLongitude,
+        storeLatitude: data.storeLatitude,
+        due: data.due,
+        menuNameList: data.menuNameList
+    };
+
+    try {
+        const res = await orderApi('api/v1/Order/history', 'post', userRequestDto);
+        return res.data;
+    } catch (error) {
+        console.error('Error creating order:', error);
+        throw error;
+    }
+};
+
+export const searchHistory = async ({ consumerId }) => {
+    try {
+        const res = await orderApi(`api/v1/Order/consumer/${consumerId}/history`, 'get');
+        console.log(res.data)
+        return res.data;
+    } catch (error) {
+        console.error('Error fetching order history:', error);
+        throw error;
+    }
 };
