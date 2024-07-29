c953d26e8d5c5a429cd8bb32b6faec2faabe9c7b
Buja <71882579+m48483@users.noreply.github.com>
2024-07-29 10:20:59 +0900

    Revert "uri 변경"

--- api/authApi.js
+++ api/authApi.js
@@ -1,10 +1,10 @@
 import {api} from "../config/AuthNetwork";
 
 export const getInfo = async (data) => {
-  const res = await api("/api/v1/customers", "get",data);
+  const res = await api("/customers", "get",data);
   return res;
 };
 export const updateInfo = async (data) => {
-  const res = await api("/api/v1/customers", "put",data);
+  const res = await api("/customers", "put",data);
   return res;
 };
