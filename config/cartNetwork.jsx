3f07459a26b60ae52bc9a04c07e53c42e0cfbaf2
giraffenostrils <dreamthre@gamil.com>
2024-08-02 10:46:37 +0900

    oderUpdate

--- config/cartNetwork.jsx
+++ config/cartNetwork.jsx
@@ -3,7 +3,7 @@ export const storeApi = async (url, method, body, params) => {
   const res = await axios({
     url,
     method,
-    baseURL: "http://192.168.0.26:8080", // baseURL
+    baseURL: "http:/34.30.133.165", // baseURL
     data: body,
     params: params,
   });
