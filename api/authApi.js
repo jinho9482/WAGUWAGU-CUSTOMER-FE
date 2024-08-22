1dc435d88ecc3d6324040eda1ab6909a3abfe330
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-08-22 17:16:29 +0900

    [Fix] endpoint for auth

--- api/authApi.js
+++ api/authApi.js
@@ -1,11 +1,11 @@
-import {api} from "../config/AuthNetwork";
+import { api } from "../config/AuthNetwork";
 
 //  fds
 export const getInfo = async (data) => {
-  const res = await api("/api/v1/customers", "get",data);
+  const res = await api("/api/v1/auth/customers", "get", data);
   return res;
 };
 export const updateInfo = async (data) => {
-  const res = await api("/api/v1/customers", "put",data);
+  const res = await api("/api/v1/auth/customers", "put", data);
   return res;
 };
