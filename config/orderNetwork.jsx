e73a461b3252c5de81c5e810e5004c8f3449c6aa
giraffenostrils <dreamthre@gamil.com>
2024-08-22 20:58:07 +0900

    a

--- config/orderNetwork.jsx
+++ config/orderNetwork.jsx
@@ -7,7 +7,7 @@ export const orderApi = async (url, method, body, params) => {
     url,
     method,
     // baseURL: "http://192.168.0.20:8080", // baseURL
-    baseURL: "https://waguwagu.shop",
+    baseURL: "http://34.44.29.146",
     data: body,
     params: params,
     headers: {
